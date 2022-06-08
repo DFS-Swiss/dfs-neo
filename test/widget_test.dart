7a73907ba832563fdba30efa460089cb72d6d5ac
tilk48 <tilman.kieselbach@me.com>
2022-06-08 17:01:56 +0200

    added dummy test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,4 +4,11 @@
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    expect(1, 1);
+  });
+}
