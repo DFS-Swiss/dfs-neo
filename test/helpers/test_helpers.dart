c6e0f5c752c7b7d95df297af853a71031f939e8c
AnHere <andre@herecki.de>
2022-07-18 17:45:52 +0200

    Got tests to work

--- test/helpers/test_helpers.dart
+++ test/helpers/test_helpers.dart
@@ -1,11 +1,13 @@
 import 'package:mockito/annotations.dart';
 import 'package:neo/service_locator.dart';
+import 'package:neo/services/app_state_service.dart';
 import 'package:neo/services/cognito_service.dart';
 
 import 'test_helpers.mocks.dart';
 
 @GenerateMocks([], customMocks: [
   MockSpec<CognitoService>(returnNullOnMissingStub: true),
+  MockSpec<AppStateService>(returnNullOnMissingStub: true),
 ])
 MockCognitoService getAndRegisterCognitoService() {
   _removeRegistrationIfExists<CognitoService>();
@@ -14,12 +16,21 @@ MockCognitoService getAndRegisterCognitoService() {
   return service;
 }
 
+MockAppStateService getAndRegisterAppStateService() {
+  _removeRegistrationIfExists<AppStateService>();
+  final service = MockAppStateService();
+  locator.registerSingleton<AppStateService>(service);
+  return service;
+}
+
 void registerServices() {
   getAndRegisterCognitoService();
+  getAndRegisterAppStateService();
 }
 
 void unregisterServices() {
   locator.unregister<CognitoService>();
+  locator.unregister<AppStateService>();
 }
 
 void _removeRegistrationIfExists<T extends Object>() {
--- test/helpers/test_helpers.mocks.dart
+++ test/helpers/test_helpers.mocks.dart
@@ -3,9 +3,12 @@
 // Do not manually edit this file.
 
 import 'dart:async' as _i3;
+import 'dart:ui' as _i7;
 
 import 'package:amazon_cognito_identity_dart_2/cognito.dart' as _i4;
 import 'package:mockito/mockito.dart' as _i1;
+import 'package:neo/enums/app_state.dart' as _i6;
+import 'package:neo/services/app_state_service.dart' as _i5;
 import 'package:neo/services/cognito_service.dart' as _i2;
 
 // ignore_for_file: type=lint
@@ -35,6 +38,9 @@ class MockCognitoService extends _i1.Mock implements _i2.CognitoService {
       super.noSuchMethod(
           Invocation.method(#createAndAuthenticateUser, [username, password]));
   @override
+  dynamic confirmRegistration(String? code) =>
+      super.noSuchMethod(Invocation.method(#confirmRegistration, [code]));
+  @override
   bool isUserPresent() =>
       (super.noSuchMethod(Invocation.method(#isUserPresent, []),
           returnValue: false) as bool);
@@ -46,4 +52,40 @@ class MockCognitoService extends _i1.Mock implements _i2.CognitoService {
       (super.noSuchMethod(Invocation.method(#getCurrentPoolUser, []),
               returnValue: Future<_i4.CognitoUser?>.value())
           as _i3.Future<_i4.CognitoUser?>);
+  @override
+  dynamic registerUser(String? userName, String? email, String? password) =>
+      super.noSuchMethod(
+          Invocation.method(#registerUser, [userName, email, password]));
+}
+
+/// A class which mocks [AppStateService].
+///
+/// See the documentation for Mockito's code generation for more information.
+class MockAppStateService extends _i1.Mock implements _i5.AppStateService {
+  @override
+  _i6.AppState get state => (super.noSuchMethod(Invocation.getter(#state),
+      returnValue: _i6.AppState.signedOut) as _i6.AppState);
+  @override
+  set state(_i6.AppState? newState) =>
+      super.noSuchMethod(Invocation.setter(#state, newState),
+          returnValueForMissingStub: null);
+  @override
+  bool get hasListeners =>
+      (super.noSuchMethod(Invocation.getter(#hasListeners), returnValue: false)
+          as bool);
+  @override
+  void addListener(_i7.VoidCallback? listener) =>
+      super.noSuchMethod(Invocation.method(#addListener, [listener]),
+          returnValueForMissingStub: null);
+  @override
+  void removeListener(_i7.VoidCallback? listener) =>
+      super.noSuchMethod(Invocation.method(#removeListener, [listener]),
+          returnValueForMissingStub: null);
+  @override
+  void dispose() => super.noSuchMethod(Invocation.method(#dispose, []),
+      returnValueForMissingStub: null);
+  @override
+  void notifyListeners() =>
+      super.noSuchMethod(Invocation.method(#notifyListeners, []),
+          returnValueForMissingStub: null);
 }
